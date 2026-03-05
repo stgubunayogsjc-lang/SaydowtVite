d5aa365b95259f6c980fe8d185965263eaf80727
SaydowtVite <stgu.bunayog.sjc@phinmaed.com>
2026-03-05 01:29:20 -0500

    Added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,25 @@ function App() {
   const [count, setCount] = useState(0)
 
   return ( 
-  <h1 className="text-5xl bg-sky-500 mx-100 my-50">Hello Bos</h1>
-  
+  <div className="navbar bg-base-100 shadow-sm">
+  <div className="flex-1">
+    <a className="btn btn-ghost text-xl">Hello Bos</a>
+  </div>
+  <div className="flex-none">
+    <ul className="menu menu-horizontal px-1">
+      <li><a>Link</a></li>
+      <li>
+        <details>
+          <summary>Parent</summary>
+          <ul className="bg-base-100 rounded-t-none p-2">
+            <li><a>Link 1</a></li>
+            <li><a>Link 2</a></li>
+          </ul>
+        </details>
+      </li>
+    </ul>
+  </div>
+</div>
   )
 }
 
