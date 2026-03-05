4a8d4d7b2d30fb37ae19d8b3582f10c9f9844b2e
SaydowtVite <stgu.bunayog.sjc@phinmaed.com>
2026-03-05 00:55:22 -0500

    inital changes

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return ( 
+  <h1 className="text-5xl bg-sky-500 mx-100 my-50">Hello Bos</h1>
+  
+  )
+}
+
+export default App
